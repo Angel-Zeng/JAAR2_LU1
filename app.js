88f56840cbac03fe091835186d53a31ad8759096
Angel Zeng <angelzeng08@hotmail.com>
2025-09-16 14:54:59 +0200

    Controller, Dao, Router initialization
    fuck azure

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 const logger = require('./util/logger')
 
+const overviewRouter = require('./src/routes/overviewRouter');
 const indexRouter = require('./src/routes/index');
 const usersRouter = require('./src/routes/users');
 
+const dotenv = require('dotenv');
 const app = express();
+dotenv.config(); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/overview', overviewRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
---
+++ src/routes/overviewRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+// const overviewController = require('../controllers/overview.controller');
+
+// const logger = require('../util/logger.js');
+
+
+
+// router routes defintion, where you put the function to be excecuted behind the comma (Like, "movieController.listMovies")
+router.get('/', )
+
+
+
+
+
+
+// Routes
+// router.get("/hello", (req, res) => {
+//   logger.info("GET request on /hello");
+//   res.send("<h1>Hello World</h1>");
+// });
+
+// router.get("/hel", (req, res) => {
+//   logger.info('GET request on "/hel"');
+//   res.send("Welkom op de homepage");
+// });
+
+// Export the router (no listen here!)
+module.exports = router;
