5699f5fdf937f8d351452d9dbe51d8456c643731
Angel Zeng <angelzeng08@hotmail.com>
2025-09-17 12:40:20 +0200

    Oh my god films/index in mijn controller???

--- src/controllers/filmsController.js
+++ src/controllers/filmsController.js
@@ -7,7 +7,7 @@ async function list(req, res, next) {
   try {
     const { q, year, genre, actor } = req.query;
     const films = await filmsService.searchFilms({ q, year, genre, actor });
-    res.render('films/index', { title: 'Films', films, filters: { q, year, genre, actor } });
+    res.render('films', { title: 'Films', films, filters: { q, year, genre, actor } });
   } catch (err) { next(err); }
 }
 
@@ -15,7 +15,7 @@ async function details(req, res, next) {
   try {
     const film = await filmsService.getFilmById(req.params.id);
     if (!film) return res.status(404).render('error', { message: 'Film niet gevonden' });
-    res.render('films/details', { title: film.title, film });
+    res.render('films', { title: film.title, film });
   } catch (err) { next(err); }
 }
 
